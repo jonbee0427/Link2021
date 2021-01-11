6c750ccc46148e7bb26fb30767977c2d75f83c26
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2021-01-12 00:14:41 +0900

    test

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,47 +4,42 @@ import 'package:flutter/material.dart';
 import 'package:link_ver1/services/database_service.dart';
 import 'package:link_ver1/widgets/message_tile.dart';
 
+//주석
 class ChatPage extends StatefulWidget {
-
   final String groupId;
   final String userName;
   final String groupName;
 
-  ChatPage({
-    this.groupId,
-    this.userName,
-    this.groupName
-  });
+  ChatPage({this.groupId, this.userName, this.groupName});
 
   @override
   _ChatPageState createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  
   Stream<QuerySnapshot> _chats;
   TextEditingController messageEditingController = new TextEditingController();
 
-  Widget _chatMessages(){
+  Widget _chatMessages() {
     return StreamBuilder(
       stream: _chats,
-      builder: (context, snapshot){
-        return snapshot.hasData ?  ListView.builder(
-          dragStartBehavior: DragStartBehavior.down,
-          padding: EdgeInsets.only(bottom: 80),
-          itemCount: snapshot.data.documents.length,
-          itemBuilder: (context, index){
-            print(index);
-            return MessageTile(
-              message: snapshot.data.documents[index].data()["message"],
-              sender: snapshot.data.documents[index].data()["sender"],
-              sentByMe: widget.userName == snapshot.data.documents[index].data()["sender"],
-              now: snapshot.data.documents[index].data()["time"],
-            );
-          }
-        )
-        :
-        Container();
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                dragStartBehavior: DragStartBehavior.down,
+                padding: EdgeInsets.only(bottom: 80),
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  print(index);
+                  return MessageTile(
+                    message: snapshot.data.documents[index].data()["message"],
+                    sender: snapshot.data.documents[index].data()["sender"],
+                    sentByMe: widget.userName ==
+                        snapshot.data.documents[index].data()["sender"],
+                    now: snapshot.data.documents[index].data()["time"],
+                  );
+                })
+            : Container();
       },
     );
   }
@@ -96,35 +91,27 @@ class _ChatPageState extends State<ChatPage> {
               width: MediaQuery.of(context).size.width,
               child: Container(
                 decoration: BoxDecoration(
-                  border:Border.all(
-                      width: 1,
-                      color:basic
-                  ),
+                  border: Border.all(width: 1, color: basic),
                   color: Colors.white,
                 ),
                 padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-               // color: Colors.white,
+                // color: Colors.white,
                 child: Row(
                   children: <Widget>[
                     Expanded(
                       child: TextField(
                         controller: messageEditingController,
-                        style: TextStyle(
-                          color: Colors.black
-                        ),
+                        style: TextStyle(color: Colors.black),
                         decoration: InputDecoration(
-                          hintText: "Send a message ...",
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
-                          ),
-                          border: InputBorder.none
-                        ),
+                            hintText: "Send a message ...",
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                            border: InputBorder.none),
                       ),
                     ),
-
                     SizedBox(width: 12.0),
-
                     GestureDetector(
                       onTap: () {
                         _sendMessage();
@@ -133,10 +120,10 @@ class _ChatPageState extends State<ChatPage> {
                         height: 50.0,
                         width: 50.0,
                         decoration: BoxDecoration(
-                          color: basic,
-                          borderRadius: BorderRadius.circular(50)
-                        ),
-                        child: Center(child: Icon(Icons.send, color: Colors.white)),
+                            color: basic,
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Center(
+                            child: Icon(Icons.send, color: Colors.white)),
                       ),
                     )
                   ],
