bf494208812dfa783603ff5d6a87925085929994
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2021-01-14 15:40:53 +0900

    김신 코드 이식

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:link_ver1/helper/helper_functions.dart';
+import 'package:link_ver1/pages/ResetPasswordPage.dart';
 import 'package:link_ver1/pages/home_page.dart';
 import 'package:link_ver1/pages/register_page.dart';
 import 'package:link_ver1/pages/verificationPage.dart';
@@ -98,106 +99,155 @@ class _SignInPageState extends State<SignInPage> {
     return _isLoading
         ? Loading()
         : Scaffold(
-            body: Form(
-            key: _formKey,
-            child: Container(
-              color: Color.fromARGB(250, 247, 162, 144),
-              child: ListView(
-                padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 80.0),
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text("H-Link",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40.0,
-                              fontWeight: FontWeight.bold)),
-                      SizedBox(height: 30.0),
-                      Text("Sign In",
-                          style:
-                              TextStyle(color: Colors.white, fontSize: 25.0)),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        style: TextStyle(color: Colors.white),
-                        decoration: textInputDecoration.copyWith(
-                            labelText: 'Student ID'),
-                        // validator: (val) {
-                        //   return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? null : "Please enter a valid email";
-                        // },
-
-                        onChanged: (val) {
-                          setState(() {
-                            email = val + '@handong.edu';
-                          });
-                        },
-                      ),
-                      SizedBox(height: 15.0),
-                      TextFormField(
-                        style: TextStyle(color: Colors.white),
-                        decoration:
-                            textInputDecoration.copyWith(labelText: 'Password'),
-                        validator: (val) => val.length < 6
-                            ? 'Password not strong enough'
-                            : null,
-                        obscureText: true,
-                        onChanged: (val) {
-                          setState(() {
-                            password = val;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 20.0),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50.0,
-                        child: RaisedButton(
-                            elevation: 0.0,
-                            color: Colors.pink[300],
-                            // Color.fromARGB(300, 247, 162, 144),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30.0)),
-                            child: Text('Sign In',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16.0)),
-                            onPressed: () {
-                              print('Signin started!');
-                              _onSignIn();
-                            }),
-                      ),
-                      SizedBox(height: 10.0),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50.0,
-                        child: RaisedButton(
-                            elevation: 0.0,
-                            color: Colors.pink[300],
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30.0)),
-                            child: Text('register',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16.0)),
-                            onPressed: () {
-                              print('register started!');
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => RegisterPage()));
-                            }),
-                      ),
-                      SizedBox(height: 10.0),
-                      Text.rich(
-                        TextSpan(
-                          text: "Don't have an account? ",
-                          style: TextStyle(color: Colors.white, fontSize: 14.0),
-                        ),
-                      ),
-                      Text(error,
-                          style: TextStyle(color: Colors.red, fontSize: 14.0)),
-                    ],
-                  ),
-                ],
-              ),
+            appBar: AppBar(
+              backgroundColor: Color.fromARGB(250, 247, 162, 144),
+              elevation: 0,
             ),
-          ));
+            body: Form(
+              key: _formKey,
+              child: Container(
+                color: Color.fromARGB(250, 247, 162, 144),
+                child: ListView(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text("H-Link",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.bold)),
+                        SizedBox(height: 30.0),
+                        Text("Sign In",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 25.0)),
+                        SizedBox(height: 50.0),
+                        Row(
+                          children: [
+                            Flexible(
+                              flex: 2,
+                              child: Column(
+                                children: [
+                                  TextFormField(
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: textInputDecoration.copyWith(
+                                        labelText: 'Student ID'),
+                                    // validator: (val) {
+                                    //   return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? null : "Please enter a valid email";
+                                    // },
+                                    onChanged: (val) {
+                                      setState(() {
+                                        email = val + '@handong.edu';
+                                      });
+                                    },
+                                  ),
+                                  SizedBox(height: 15.0),
+                                  TextFormField(
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: textInputDecoration.copyWith(
+                                        labelText: 'Password'),
+                                    validator: (val) => val.length < 6
+                                        ? 'Password not strong enough'
+                                        : null,
+                                    obscureText: true,
+                                    onChanged: (val) {
+                                      setState(() {
+                                        password = val;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 15.0),
+                            Flexible(
+                              flex: 1,
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: 100.0,
+                                child: RaisedButton(
+                                    elevation: 0.0,
+                                    color: Colors.pink[300],
+                                    // Color.fromARGB(300, 247, 162, 144),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30.0)),
+                                    child: Text('로그인',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16.0)),
+                                    onPressed: () {
+                                      print('Signin started!');
+                                      _onSignIn();
+                                    }),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 40.0),
+                        Row(
+                          children: [
+                            Flexible(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: 50.0,
+                                child: RaisedButton(
+                                    elevation: 0.0,
+                                    color: Colors.pink[300],
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30.0)),
+                                    child: Text('비밀번호 찾기',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16.0)),
+                                    onPressed: () {
+                                      print('비밀번호 찾기 started!');
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ResetPasswordPage()));
+                                    }),
+                              ),
+                            ),
+                            SizedBox(width: 10.0),
+                            Flexible(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: 50.0,
+                                child: RaisedButton(
+                                    elevation: 0.0,
+                                    color: Colors.pink[300],
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30.0)),
+                                    child: Text('회원가입',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16.0)),
+                                    onPressed: () {
+                                      print('register started!');
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  RegisterPage()));
+                                    }),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(error,
+                            style:
+                                TextStyle(color: Colors.red, fontSize: 14.0)),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ));
   }
 }
